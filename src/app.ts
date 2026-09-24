969a3e76dd686d221a70915003d8f4f9a80c534f
mateus23-oliveira <mateus17333@gmail.com>
2026-09-24 19:17:19 -0300

    criando rota de evento

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import userRoutes from "./routes/userRoutes";
 import authRoutes from "./routes/authRoutes";
 import animalRoutes from "./routes/animalRoutes"; 
+import formularioRoutes from "./routes/formularioRoutes"
 
 const app = express();
 
@@ -20,5 +22,15 @@ app.get("/", (req, res) => {
 app.use("/auth", authRoutes);
 
 app.use("/usuarios", userRoutes);
+
+app.use("/animais", animalRoutes);
+
+app.use("/formularios", formularioRoutes);
+
+
+app.use(
+  "/uploads",
+  express.static(path.resolve("uploads"))
+);
 
 export default app;
